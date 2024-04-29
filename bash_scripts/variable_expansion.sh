6334a61802b4892084e95af79c4b71783f55ee5c
Kurt Butler <guitarnut06@yahoo.com>
2024-04-28 21:34:56 -0500

    Added comments for variable expansion

--- bash_scripts/variable_expansion.sh
+++ bash_scripts/variable_expansion.sh
@@ -8,11 +8,12 @@ myvar2=""
 echo "myvar1:  $myvar1"
 echo "myvar2:  $myvar2"
 
-myvar2="testing"
+#myvar2="testing"
 echo
 
 # Show a "default" value if null
 echo "myvar1:  ${myvar1:-empty}"
+# Show and set a value if null
 echo "myvar2:  ${myvar2:=empty2}"
 echo
 echo "myvar1:  $myvar1"
